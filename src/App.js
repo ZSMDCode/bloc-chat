0da397a9c64cd41107c3a4a9d24f3c194dc3b2dc
Zart Seri <zartseri@yahoo.com>
2018-07-31 13:29:54 -0400

    chat rooms checkpoint for slack help

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
+import * as firebase from 'firebase';
 import React, { Component } from 'react';
+import RoomList from './components/RoomList';
 import logo from './logo.svg';
 import './App.css';
-
+src="https://www.gstatic.com/firebasejs/5.3.0/firebase.js"
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAE3TkOoZoRyBQnlg2JTWcsF9IZaounnXU",
+    authDomain: "blocchatfirebase.firebaseapp.com",
+    databaseURL: "https://blocchatfirebase.firebaseio.com",
+    projectId: "blocchatfirebase",
+    storageBucket: "blocchatfirebase.appspot.com",
+    messagingSenderId: "282531310560"
+  };
+  firebase.initializeApp(config);
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <RoomList />
       </div>
     );
   }
