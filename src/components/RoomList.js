74950004c968619d40da5ee0e2e7e110747e7015
Zart Seri <zartseri@yahoo.com>
2018-08-02 14:08:04 -0400

    Checkpoint 3 Create: Completed

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,39 +1,44 @@
 import React, { Component } from 'react';
 class RoomList extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        rooms: [],
-        newRoomName: ''
-      };
-      this.roomsRef = this.props.firebase.database().ref('rooms');
-      }
-      handleSubmit(e) {
-        e.preventDefault();
-        this.roomsRef.push({
-        newRoomName: e.target.value
-        });
-      }
-      componentDidMount() {
-        this.roomsRef.on('child_added', snapshot => {
-          const room = snapshot.val();
-          room.key = snapshot.key;
-          this.setState({ rooms: this.state.rooms.concat( room ) })
-        });
-      }
-      render() {
-        return (
-          <div>
-          {
-          this.state.rooms.map((room, index) =>
-          <div key={index}>{room.name}</div>
-        )}
-        <form onSubmit={ (e) => this.handleSubmit(this.state.newRoomName) }>
-        <input type="text" value={ this.state.newRoomName }/>
-        <input type="submit"/>
-        </form>
-        </div>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      rooms: [],
+      newRoomName: ''
+    };
+    this.roomsRef = this.props.firebase.database().ref('rooms');
   }
+  handleSubmit(newRoomName) {
+    this.roomsRef.push({
+      name:newRoomName
+    });
+  }
+  handleChange(e){
+    this.setState({ newRoomName: e.target.value })
+  }
+  componentDidMount() {
+    this.roomsRef.on('child_added', snapshot => {
+      const room = snapshot.val();
+      room.key = snapshot.key;
+      this.setState({ rooms: this.state.rooms.concat( room ) })
+    });
+  }
+  render() {
+    return (
+      <div>
+      {
+        this.state.rooms.map((room, index) =>
+        <div key={index}>{room.name}</div>
+      )}
+      <form onSubmit={ (e) => {
+        e.preventDefault();
+        this.handleSubmit(this.state.newRoomName) }
+      }>
+      <input type="text" value={ this.state.newRoomName }  onChange={ (e) => this.handleChange(e)}/>
+      <input type="submit"/>
+      </form>
+      </div>
+    );
+  }
+}
 export default RoomList;
