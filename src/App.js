a005e1487f0cfcd2e60e68b1259e710168fc1dd2
Zart Seri <zartseri@yahoo.com>
2018-08-20 13:03:35 -0400

    WIP for Slack help - Username

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import * as firebase from 'firebase';
 import React, { Component } from 'react';
 import RoomList from './components/RoomList';
 import MessageList from './components/MessageList';
+import User from './components/User';
 import './App.css';
 var config = {
   apiKey: "AIzaSyAE3TkOoZoRyBQnlg2JTWcsF9IZaounnXU",
@@ -16,17 +17,24 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeRoom: ''
+      activeRoom: '',
+      activeUser: ''
     };
   }
   handleRoomClick = (room) => {
       this.setState({ activeRoom: room});
     }
+
+  setUser = (user) => {
+    this.setState({activeUser: user});
+  }
+
     render() {
       return (
         <div className="App">
         <RoomList firebase={firebase} activeRoom={this.state.activeRoom} handleRoomClick={this.handleRoomClick}/>
         <MessageList firebase={firebase} activeRoom={this.state.activeRoom}/>
+        <User firebase={firebase} setUser={this.setUser} activeUser={this.state.activeUser}/>
         </div>
       );
     }
---
+++ src/components/User.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+class User extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [],
+    };
+    this.usersRef = this.props.firebase.database().ref('users');
+  }
+  componentDidMount() {
+    this.props.firebase.auth().onAuthStateChanged( user => {
+    this.props.setUser(user);
+  });
+}
+  authenticate(){
+    const provider = new this.props.firebase.auth.GoogleAuthProvider();
+    this.props.firebase.auth().signInWithPopup( provider );
+    console.log("logged in" + this.props.activeUser);
+  }
+  signOut(){
+    this.props.firebase.auth().signOut();
+    console.log("logged out")
+  }
+  render() {
+    console.log("active user = " + this.props.activeUser)
+    return (
+      <div>
+      <input type="button" value={"Sign in"} onClick={() => {this.authenticate()}}/>
+      <input type="button" value={"Sign out"} onClick={() => {this.signOut()}}/>
+      </div>
+    );
+  }
+}
+export default User;
